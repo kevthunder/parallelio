cd08b6aedcbb29b5eb214d41132de74500f6cfc1
Kevin Giguere <kevin.lacasse@ssense.com>
2020-01-27 17:23:17 -0500

    AirlockGenerator

---
+++ test/generators/AirlockGeneratorTest.js
@@ -0,0 +1,9 @@
+const assert = require('chai').assert
+const StarMapGenerator = require('../../lib/generators/AirlockGenerator')
+const Alea = require('seedrandom/lib/alea')
+
+describe('AirlockGenerator', function () {
+  it('can create airlock', function () {
+
+  })
+})
